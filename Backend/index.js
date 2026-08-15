100a21183af677026f613e58e8be8ae4087f4f03
IB-Tanuj <tarun.3240910@gmail.com>
2026-08-16 01:03:24 +0530

    visit

--- Backend/index.js
+++ Backend/index.js
@@ -10,6 +10,7 @@ import flightRoutes from "./routes/flightRoutes.js";
 import vehicleRoutes from "./routes/vehicleRoutes.js";
 import imageRoutes from "./routes/imageRoutes.js";
 import hotelRoutes from "./routes/hotelRoutes.js";
+import bestTimeRoutes from "./routes/bestTimeRoutes.js";
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.use("/api/flights", flightRoutes);
 app.use("/api/vehicle", vehicleRoutes);
 app.use("/api/images", imageRoutes);
 app.use("/api/hotels", hotelRoutes);
+app.use("/api/best-time", bestTimeRoutes);
 
 app.get("/", (req, res) => {
   res.json({
